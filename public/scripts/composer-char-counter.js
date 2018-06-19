5259627d5aff9f5f78b5b1e71254d20719d046b1
giovaniv <giovaniv@gmail.com>
2018-06-19 19:12:36 +0000

    char counter - task 6 - working

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
-  const limitChars = 140;
-  let textLength = document.getElementsByName("text")[0].value;
+  const limitChars = 10;
   let counter = document.getElementsByClassName('counter')[0].innerHTML;
 
   if (!counter) {
@@ -9,18 +8,34 @@ $(document).ready(function() {
     $('.counter').text(limitChars);
   }
 
-  $( "section textarea" ).keypress(function( event ) {
-    let typedText = event.target.value;
-    let newTextLength = typedText.length;
-    if (newTextLength >= limitChars) {
-      $('section textarea').text(typedText);
-    }
-  });
+  $("textarea").keyup(function( event ) {
 
-  $( "section textarea" ).keydown(function( event ) {
-    if (counter >= 0) {
-      $('.counter').text(counter--);
+    let keyCode = event.keyCode;
+    let text = this.value;
+    let length = this.value.length;
+
+    if (!length) {
+      $('.counter').text(limitChars);
+    } else {
+      counter = limitChars - length;
+      if (keyCode === 8) {
+        $('.counter').text(counter++);
+      } else {
+        $('.counter').text(counter--);
+      }
     }
+
+    if (length > limitChars) {
+      $('.counter').css("color", "red");
+    } else {
+      $('.counter').css("color", "black");
+    }
+
   });
 
 });
+
+
+
+
+
