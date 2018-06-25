77cb17fbe843385a6a7a26e8b2b7a6083a15386f
giovaniv <giovaniv@gmail.com>
2018-06-25 02:22:24 +0000

    updated logout endpoint

--- server/routes/users.js
+++ server/routes/users.js
@@ -90,67 +90,9 @@ module.exports = function(DataHelpers) {
 
   // Logout the user and clean cookie
   usersRoutes.post('/logout', (req, res) => {
-    // req.session = null;
-    console.log('login');
-    res.send("login");
+    req.session = null;
+    res.status(201).render('index');
   });
-
- // ==========================================================
-
-
-
-
-  // usersRoutes.get("/register", function(req, res) {
-  //   if
-
-
-  //   DataHelpers.getTweets((err, tweets) => {
-  //     if (err) {
-  //       res.status(500).json({ error: err.message });
-  //     } else {
-  //       res.json(tweets);
-  //     }
-  //   });
-  // });
-
-  // usersRoutes.get("/", function(req, res) {
-  //   if (!req.body.text) {
-  //     res.status(400).json({ error: 'invalid request: no data in POST body'});
-  //     return;
-  //   }
-
-  //   const user = req.body.user ? req.body.user : userHelper.generateRandomUser();
-  //   const tweet = {
-  //     user: user,
-  //     content: {
-  //       text: req.body.text
-  //     },
-  //     created_at: Date.now(),
-  //     likes: 0,
-  //     liked: false
-  //   };
-
-  //   DataHelpers.saveTweet(tweet, (err) => {
-  //     if (err) {
-  //       res.status(500).json({ error: err.message });
-  //     } else {
-  //       res.status(201).send();
-  //     }
-  //   });
-  // });
-
-  // // Route to update likes of a tweet
-  // usersRoutes.post("/:id", function(req, res) {
-  //   let uid = req.params.id;
-  //   let liked = req.body.liked;
-  //   DataHelpers.saveLikes(uid, liked, (err) => {
-  //     if (err) {
-  //       res.status(500).json({ error: err.message });
-  //     } else {
-  //       res.status(201).send();
-  //     }
-  //   });
-  // });
 
   return usersRoutes;
 
